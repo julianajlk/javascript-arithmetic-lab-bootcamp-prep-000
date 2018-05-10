2eccc0e9c22701849bf4f8d948484895841a57fb
Juliana Kang <julianajlk@gmail.com>
2018-05-10 14:40:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ function add(a, b) {
   return number;
 }
 
-function subtract() {
+function subtract(a, b) {
+ number = a - b;
+ return number;
+}
+
+function multiply(a, b) {
 
 }
 
-function multiply() {
-
-}
-
-function divide() {
+function divide(a, b) {
 
 }
